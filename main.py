6362466a4d6e323653fc425b57d24f0b818c31d8
George Lawrence <georgeclawrence@me.com>
2023-07-12 13:40:07 -0400

    more changes

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 from spotify import getAlJkPlaylist
-from youtube import searchVideo, getVideoDuration
+from youtube import searchVideo, getVideoDuration, searchVideoRequests
 from youtube_dl import downloadVideo
 
 if __name__ == "__main__":
     tracks = getAlJkPlaylist()
+    cont = True
+    contFrom = "Glory Days by Bruce Springsteen"
 
     for i, track in enumerate(tracks):
+        if contFrom is not None and contFrom == track['title']:
+            cont = False 
+            print(f"CONTINUING FROM: {contFrom}")
+            continue 
+        if cont:
+            continue
         if (i + 1) % 10 == 0:
             print(f"PROGRESS: {i + 1}/{len(tracks)}")
         videoId = searchVideo(track['title'])
         if videoId is None:
-            print(f"ERROR: {track['title']} not found")
-            continue
-        duration = getVideoDuration(videoId)
-
-        # if percent difference of duration and track[duration] is greater than 10%, print error
-        if abs(duration - track['duration']) / track['duration'] > 0.1:
-            print(f"ERROR: {track['title']} has a duration of {track['duration']} but the video has a duration of {duration}")
-            continue
-        downloadVideo(videoId)
+            videoId = searchVideoRequests(track['title'], track['duration'])
+            if videoId is None:
+                continue
+            downloadVideo(videoId)
+        else:
+          duration = getVideoDuration(videoId)
+          # if percent difference of duration and track[duration] is greater than 10%, print error
+          if abs(duration - track['duration']) / track['duration'] > 0.1:
+              print(f"ERROR: {track['title']} has a duration of {track['duration']} but the video has a duration of {duration}")
+              continue
+          downloadVideo(videoId)
             
--- youtube.py
+++ youtube.py
@@ -1,12 +1,13 @@
 import os 
 from dotenv import load_dotenv
 import requests
+import json
+import re
 
 load_dotenv()
 
 # search Youtube v3 Data API for a video
 def searchVideo(query):
-    
     # get API key from .env
     API_KEY = os.environ.get("YOUTUBE_API_KEY")
     
@@ -15,13 +16,16 @@ def searchVideo(query):
     
     # send request
     response = requests.get(request)
-    
+    print(response.json())
     # return video id
     if response.status_code != 200:
         print(f"ERROR: {response.status_code, response.reason}")
         return None
-
-    return response.json()['items'][0]['id']['videoId']
+    try:
+        return response.json()['items'][0]['id']['videoId']
+    except:
+        print(f"ERROR with JSON: {response.json()}")
+        return None
 
 # get video duration
 def getVideoDuration(videoId):
@@ -67,5 +71,43 @@ def iso8601toMilliseconds(iso8601):
         milliseconds += seconds * 1000
 
     return milliseconds
+
+def lengthTextToMilliseconds(lengthText):
+    parts = lengthText.split(':')
+    if len(parts) == 3:
+        hours = int(parts[0])
+        minutes = int(parts[1])
+        seconds = int(parts[2])
+    else:
+        hours = 0
+        minutes = int(parts[0])
+        seconds = int(parts[1])
+    return hours * 60 * 60 * 1000 + minutes * 60 * 1000 + seconds * 1000
+    
+
+def searchVideoRequests(query, ogDuration):
+    params = {'search_query':query.replace(' ', '+')}
+    r = requests.get('https://www.youtube.com/results', params=params)
+    js = json.loads(re.findall('ytInitialData.*?({.*?});', r.text)[0])
+    # get video id and duration for the video in the first 3 results closest to the duration of the song
+    minDif = 1.
+    minInd = 0
+    for i in range(3):
+        try:
+            videoInfo = js['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][i]['videoRenderer']
+        except:
+            print(f"Unable to find {query} at index {i}")
+            continue
+        videoDuration = lengthTextToMilliseconds(videoInfo['lengthText']['simpleText'])
+        minDif = min(minDif, abs(videoDuration - ogDuration)/ogDuration)
+        if minDif == abs(videoDuration - ogDuration)/ogDuration:
+            minInd = i
+    if minDif > 0.05:
+        print(f"ERROR: {query} not found with manual search. Closest video has a duration of {videoDuration} compared to {ogDuration}")
+        return None
+    videoInfo = js['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents'][minInd]['videoRenderer']
+    if (minInd != 0):
+        print(f"WARNING: {query} not found at index 0. Found at index {minInd}")
+    return videoInfo['videoId']
 
 #print(getVideoDuration(searchVideo("Build Me Up Buttercup by The Foundations")))
